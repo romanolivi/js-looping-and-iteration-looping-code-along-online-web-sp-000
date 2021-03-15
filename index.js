517ac0e38dc8b3ad16901720c1c2c521203dd99d
romanolivi <romano_livi@outlook.com>
2021-03-15 13:22:14 -0400

    commit tree 08cfc30dbd39de7993793c02db69b8cf6fea3296

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(array, holiday, friends) {
+    friends = [];
+    for (let i = 0; i < array.length; i++) {
+        friends.push(`Thank you, ${array[i]}, for the wonderful ${holiday} gift!`);
+    }
+
+    return friends
+
+}
+
+writeCards(["Ada", "Tommy", "Arthur"], "surprise", friends);
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
